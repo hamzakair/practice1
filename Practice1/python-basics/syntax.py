afaf02f4f49dc09d67d29dbc30e8b6e8ce360e04
Khamza <kzhamz18@gmail.com>
2026-01-22 12:18:42 +0500

    Add synxtax examples

---
+++ Practice1/python-basics/syntax.py
@@ -0,0 +1,18 @@
+# example 1
+print("Hello, World!")
+
+# example 2
+if 5 > 2:
+    print("Five is greater than two")
+
+# example 3
+x = 10
+print(x)
+
+# example 4
+x = 5
+y = 3
+print(x + y)
+
+# example 5
+print("Python syntax example")
